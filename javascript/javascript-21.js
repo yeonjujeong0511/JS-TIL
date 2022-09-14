53dad99de5d7edabc4a8e60d453c099d2f83dce1
yeonjujoeng0511 <yeonjujeong0511@gmail.com>
2022-09-14 17:14:38 +0900

    0914 학습내용

---
+++ javascript/javascript-21.js
@@ -0,0 +1,14 @@
+let variableA = 305;
+let variableB = 305;
+let variableC = "305";
+
+if (variableA === variableB ) {
+	console.log("true");
+}
+// variableA와 variableB의 값이 일치한다면, 콘솔에 true를 출력해라
+// true
+if (variableA !== variableC ) {
+	console.log("true");
+}
+// variableA와 variableC의 값이 일치하지않는다면, 콘솔에 true를 출력해라
+// true
