5ef7a6721436a371477ebb4f972ebd1b16a2bf10
yeonju0511 <yeonjujeong0511@gmail.com>
2022-12-21 02:06:01 +0900

    불필요한 class삭제 및 코드 이해

--- hidden/app.js
+++ hidden/app.js
@@ -1,25 +1,32 @@
-const questions = document.querySelectorAll(".question");
+const projects = document.querySelectorAll(".project");
+console.log(projects);
 
-questions.forEach(function (question) {
-  // console.log(questions);
-  const btn = question.querySelector(".btn");
+projects.forEach(function (project) {
+  // console.log(projects);
+  const btn = project.querySelector(".btn");
   console.log(btn);
+  // 버튼을 클릭하면, class에 project만 존재하면, show-text 클래스 추가해서 화면에 내용을 함께 출력해주고
+  // class에 project와 show-text가 함께 있으면, 클래스를 지워줌으로써 다시 화면에서 사리지게 한다.
+
   btn.addEventListener("click", function () {
-    questions.forEach(function (item) {
+    projects.forEach(function (item) {
       console.log(item);
-      if (item !== question) {
+      if (item !== project) {
         item.classList.remove("show-text");
       }
     });
-    question.classList.toggle("show-text");
+    project.classList.toggle("show-text");
   });
 });
+
+// ! 위 아래 btn에 클릭 이벤트를 주어, proejct라는 클래스를 가진 것에 클래스를 추가하여 핸들링 하는 것
+// ! 위에서 아래로 가져가느냐, 아래에서 위로 올려주느냐 차이인 듯!
 
 // const btns = document.querySelectorAll(".btn");
 
 // btns.forEach(function (btn) {
 //   btn.addEventListener("click", function (e) {
-//     const question = e.currentTarget.parentElement.parentElement;
-//     question.classList.toggle("show-text");
+//     const project = e.currentTarget.parentElement.parentElement;
+//     project.classList.toggle("show-text");
 //   });
 // });
