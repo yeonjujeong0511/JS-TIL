2d3deb9a53270d55e446edd9d93b7913ed86282e
yeonju0511 <yeonjujeong0511@gmail.com>
2022-12-23 23:52:52 +0900

    메뉴 만드는 중

---
+++ menu/app.js
@@ -0,0 +1,32 @@
+const menu = [
+  {
+    id: 1,
+    title: "탄탄면공방",
+    category: "중식",
+    price: "9900",
+    img: "",
+    desc: "기본 탄탄면부터 존맛탱,땅콩향이 가득가득 고소한 탄탄면~!",
+  },
+  {
+    id: 2,
+    title: "토미야",
+    category: "일식",
+    price: "8500",
+    img: "",
+    desc: "냉우동 맛집, 무조건 돈까스랑 가라야게를 같이 시켜서 먹어야한다",
+  },
+  {
+    id: 3,
+    title: "소바노하나",
+    category: "일식",
+    price: "8500",
+    img: "",
+    desc: "소바랑 우동맛집, 개인적으로는 김치우동 굿굿! 돈까스가 아주 맛있다",
+  },
+];
+
+const menuSection = document.querySelector(".menu-section");
+
+window.addEventListener("DOMContentLoaded", function () {
+  console.log("shake and bake");
+});
